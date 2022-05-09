04dc4618b84332f6136915215734c1dec43895e3
TOVTC <veronica.tc.to@outlook.com>
2022-05-09 07:15:56 -0400

    tested all data return join logic

--- db/schema.sql
+++ db/schema.sql
@@ -1,26 +1,26 @@
-DROP TABLE IF EXISTS departments;
-DROP TABLE IF EXISTS roles;
 DROP TABLE IF EXISTS employees;
+DROP TABLE IF EXISTS roles;
+DROP TABLE IF EXISTS departments;
 
 CREATE TABLE departments (
-    id INTEGER AUTO_INCREMENT PRIMARY KEY,
-    dept_name VARCHAR(50) NOT NULL
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE roles (
-    id INTEGER AUTO_INCREMENT PRIMARY KEY,
-    job_title VARCHAR(50) NOT NULL,
-    salary INTEGER NOT NULL,
-    dept INTEGER,
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    job_title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    dept INT,
     CONSTRAINT fk_dept FOREIGN KEY (dept) REFERENCES departments(id) ON DELETE SET NULL
 );
 
 CREATE TABLE employees (
-    id INTEGER AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(50) NOT NULL,
-    last_name VARCHAR(50) NOT NULL,
-    role_id INTEGER,
-    manager_id INTEGER,
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT,
+    manager_id INT,
     CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE SET NULL,
     CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employees(id) ON DELETE SET NULL
 );
--- index.js
+++ index.js
@@ -1,28 +1,55 @@
 // SQL
-// set up folders for SQL tables
-// create databse and tables in my sql terminal before adding to .sql files
-// departments, roles, employees
-// departments: department names and id
-// roles: job title, role id, department the role belongs to, salary for the role
-// employees: id, first name, last name, job title, departments, salaries, managers
+// test table joins
 
-// departments (name and id)
-// roles table (job title, role id, foreign key to dept table, salary for role)
-// employees table (id, first name, last name, foreign key to role id + department + salary, foreign key to another employee - self-referring key)
+// SELECT
+// 	employees.id,
+// 	concat(employees.last_name, ", ", employees.first_name) AS employee_name,
+// 	roles.job_title,
+// 	roles.salary,
+// 	departments.name AS department,
+// 	concat(managers.last_name, ", ", managers.first_name) AS manager_name
+// FROM
+// 	employees
+// LEFT JOIN
+// 	roles ON employees.role_id=roles.id
+// LEFT JOIN
+// 	departments ON roles.dept=departments.id
+// LEFT JOIN
+// 	employees managers ON employees.manager_id=managers.id;
 
-// .env file
-// db/
-// db.sql
-// schema.sql
-// seeds.sql
+// SELECT
+// 	concat(employees.first_name, " ", employees.last_name) AS employee_name,
+// 	roles.job_title,
+// 	roles.salary,
+// 	departments.name AS department
+// FROM
+// 	employees
+// LEFT JOIN
+// 	roles ON employees.role_id=roles.id
+// LEFT JOIN
+// 	departments ON roles.dept=departments.id;
+
+// SELECT
+// 	concat(employees.first_name, " ", employees.last_name) AS employee_name,
+// 	concat(managers.first_name, " ", managers.last_name) AS manager_name,
+// FROM 
+// 	employees employees
+// LEFT JOIN
+// 	employees managers
+// ON
+// 	employees.manager_id=managers.id;
 
 // SERVER
-// get: departments, roles, employees
+// get: view departments
+// get: view roles
+// get: view employees
 // post: add a department
 // post: add a role
 // post: add an employee
 // put: update an employee role
+
 // router file for each type of table to handle specific types of requests?
+// view all departments, view all roles, view all employees, add a department, add a role, add an employee, and update an employee role
 
 // maybe use constructor functions to create specific objects out of inquirer responses?
 
@@ -30,6 +57,13 @@
 // stringify returned promises
 // each type of command triggers a different instance of inquirer?
 // object.data[...] - so for each item in the returned data array, join the values needed and push to inquirer options?
+
+// BONUSES
+// update employee manager
+// view employees by manager
+// view employees by department
+// delete department, roles, employees
+// view total utilized budget by department (combined salaries of all dept employees)
 
 // validations and bonuses, change to asynchronous calls
 // MySQL2 exposes a .promise() function on Connections to upgrade an existing non-Promise connection to use Promises
